91795b3ab110ff8b81c4bd912a92cd54e9287a11
javi <bzjvr@hotmail.com>
2024-09-15 20:45:40 -0700

    refactor: updated which-key config since .register was deprecated

--- init.lua
+++ init.lua
@@ -169,12 +169,12 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', { name = '[c]ode' } },
+        { '<leader>d', { name = '[c]ode' } },
+        { '<leader>r', { name = '[c]ode' } },
+        { '<leader>s', { name = '[c]ode' } },
+        { '<leader>w', { name = '[c]ode' } },
       }
     end,
   },
